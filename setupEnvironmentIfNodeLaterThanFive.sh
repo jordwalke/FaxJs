9fc5a6aac56848aaa5e0ba7e9f5d7aeee2acc40a
jordan <jwalke@jordans-MacBook-Air.local>
2011-09-24 03:02:50 -0700

    setup script for latest node version

--- setupEnvironmentIfNodeLaterThanFive.sh
+++ setupEnvironmentIfNodeLaterThanFive.sh
@@ -1,8 +1,14 @@
 # Sets up npm links to the local folders. Cd into this dir then run this.
 
 # only two dependencies Uncomment if you don't already have these
-sudo npm install connect;
+# Newest versions of node.js aren't supported with connect
+# We include a version that works for our purposes and manually inject into
+# the node_modules directory.
 sudo npm install burrito;
+
+mkdir -p ./node_modules
+unzip connect_for_modern_node.zip
+mv connect node_modules
 
 cd ./Fax;
 sudo npm link;
