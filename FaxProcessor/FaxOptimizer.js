abdaf283322cc55f54560c44742df94b6b061777
jordan <jwalke@jordans-MacBook-Air.local>
2011-10-01 15:49:43 -0700

    New Javascript AST query mini-language on top of uglify.
    A way to traverse javascript AST's using JSON and have matched
    constraints automatically be given names according to their JSON keys!
    Running it in parallel with a couple of other AST constraint matchers
    to see if they ever differ.

--- FaxProcessor/FaxOptimizer.js
+++ FaxProcessor/FaxOptimizer.js
@@ -5,6 +5,10 @@
 var sys = require('sys'),
     jsParser = require('uglify-js').parser,
     uglify = require('uglify-js').uglify;
+
+var isArrayish = function(obj) {
+  return obj && obj.push && obj.indexOf;
+};
 /**
  * Close enough for us!
  */
@@ -23,6 +27,125 @@ var isValidTailCallConstructor = function(name) {
 				 name[0] !== '_';
 };
 
+var MATCH = function(pattern) {
+  return {
+    store: function(name) {
+      return {
+        __onlyMatchingStored: pattern,
+        storeIntoName: name
+      };
+    }
+  };
+};
+
+var ANY = {
+  store: function(name) {
+    var anyStored = {
+      __anyStored: true,
+      storeIntoName: name
+    };
+    return anyStored;
+  }
+};
+
+/**
+ */
+var match = function(node, pattern, matchedSoFar) {
+  var storeIntoName, pattI;
+  matchedSoFar = matchedSoFar || {};
+  if(pattern === node) { // literals/undef/nulls - perfect match
+    return matchedSoFar;
+  } else if (node === undefined || pattern === undefined ||
+             node === null || pattern === null) {
+    // found a contradication. If the pattern was a literal null/undef then the
+    // node should have matched that exactly in the first step - else we should
+    // return false here.
+    return null;
+  } else if(pattern === ANY) {
+    return matchedSoFar;
+  } else if(pattern.__anyStored) {
+    storeIntoName = pattern.storeIntoName;
+    matchedSoFar[storeIntoName] = node;
+    return matchedSoFar;
+  } else if(pattern.hasOwnProperty('__onlyMatchingStored')) {
+    storeIntoName = pattern.storeIntoName;
+    matchedSoFar = match(node, pattern.__onlyMatchingStored, matchedSoFar);
+    if(!matchedSoFar) {
+      return matchedSoFar;
+    }
+    matchedSoFar[storeIntoName] = node;
+    return matchedSoFar;
+  } else {               // recurse and mutate matchedSoFar.
+    if (isArrayish(node) && isArrayish(pattern)) {
+      // contradiction - entire pattern doesn't match
+      if(pattern.length > node.length) {
+        return null;
+      }
+      for (pattI = 0; pattI < pattern.length; pattI++) {
+        matchedSoFar = match(node[pattI], pattern[pattI], matchedSoFar);
+        if(!matchedSoFar) {
+          return matchedSoFar;
+        }
+      }
+      return matchedSoFar;
+    }
+    return null;
+  }
+  throw "If this is ever thrown, then I vow to rewrite everything " +
+        "in Ocaml which features comprehensive pattern matching with " +
+        "exhaustiveness checks!";
+};
+
+/**
+ * When this is mature enough - release as a separate NPM utility - it's quite
+ * helpful for code processing. Kind of like a poor man's Ocaml pattern matching
+ * in javascript.
+ */
+var matchQuery = function(node, pattern) {
+  var pattI, matchResult, bindings;
+  if(pattern === node) { // literals/undef/nulls - perfect match, terminal case
+    return {matched: true, val: node};
+  } else if (node === undefined || pattern === undefined ||
+             node === null || pattern === null) {
+    // found a contradication. If the pattern was a literal null/undef then the
+    // node should have matched that exactly in the first step - else we should
+    // return false here.
+    return {matched: false};
+  } else if(pattern === ANY) {
+    // kind of like another terminal case but the terminal value might have depth to it.
+    return {matched: true, val: node};
+  } else {               // recurse and mutate matchedSoFar.
+    if (isArrayish(node) && typeof pattern === 'object') {
+      var recursiveMatchNames = Object.keys(pattern);
+      // contradiction - out pattern was trying to match many things, and the
+      // node didn't even have enough children.
+      if(recursiveMatchNames.length > node.length) {
+        return {matched: false};
+      }
+      bindings = {};
+      for (pattI = 0; pattI < recursiveMatchNames.length; pattI = pattI + 1) {
+        matchResult = matchQuery(node[pattI], pattern[recursiveMatchNames[pattI]]);
+        if(!matchResult.matched) {
+          return {matched: false};
+        } else {
+          if(matchResult.bindings) {
+            bindings[recursiveMatchNames[pattI]] = matchResult.bindings;
+            bindings[recursiveMatchNames[pattI]].__node = node[pattI];
+          } else if (matchResult.hasOwnProperty('val')) {
+            bindings[recursiveMatchNames[pattI]] = matchResult.val;
+          }
+        }
+      }
+      return {matched: true, bindings: bindings};
+    }
+    return {matched: false};
+  }
+  throw "If this is ever thrown, then I vow to rewrite everything " +
+        "in Ocaml which features comprehensive pattern matching with " +
+        "exhaustiveness checks! Not really but I should.";
+};
+
+
 
       //['call',   ['dot',        ['object', []],                                                 'DemoAppContent'],[]]]]]]
 /*                /node[1][0]\   /-----------------------node[1][1]-------------------\
@@ -34,22 +157,78 @@ var swapAndOptimizeTailConstruction = function(origNode, methodData) {
                                                                                                       origNode[2] = [optimizeTree(methodData.leftOfDot)];
   return origNode;
 };
+
 function methodDataIffMethodNode(node) {
   if(!isNonTerm(node)) {
     return null;
   }
+  // The next few lines are known to work:
   var isCall = node.length === 3 && node[0] === 'call';
   var isDotCall = isCall && isNonTerm(node[1]) && node[1].length && node[1][0] === 'dot';
   var hasDotData = isDotCall && node[1].length === 3 ;
-  // We'll be super liberal about what's to the left of the dot:
-  // Could have also said: // && isNonTerm(node[1][1]) && node[1][1].length === 2;
 
-  return hasDotData ? {
-    typeOfThingLeftOfDot: node[1][1][0],  // We don't currently care about this but helpful in future
-    leftOfDot: node[1][1],
-    methodName: node[1][2],
-    args: node[2]
-  } : null;
+  /* This is the newer experimental matcher we run them in parallel but log
+   * errors when we see discrepancies. */
+  var matched = match(node,
+    [ 'call',
+      [ 'dot', MATCH([ANY.store('typeOfThingLeftOfDot')]).store('leftOfDot'), ANY.store('methodName') ], ANY.store('args')
+    ]
+  );
+
+  /**
+   * This is yet another experimental ast query engine that we'll run in
+   * parallel to see if it is as accurate as manual checking.
+   */
+  var nodeConstraint = {
+      nodeTypeShouldBe: 'call',
+      callBase: {
+        operationShouldBe: 'dot',
+        leftOfDot: {
+          typeOfThingLeftOfDot: ANY
+        },
+        methodName: ANY
+      },
+      args: ANY
+  };
+  var astQueryMatch = matchQuery(node, nodeConstraint);
+
+  // We only check discrepancies in one of the directions - we should check both.
+  if(matched) {
+    if(matched.typeOfThingLeftOfDot !== node[1][1][0]) {
+      console.log("ERROR: Found match error left of dot: if you see this, let someone know");
+    }
+    if(astQueryMatch.bindings.callBase.leftOfDot.typeOfThingLeftOfDot !== matched.typeOfThingLeftOfDot) {
+      console.log("ERROR: AST - astQueryMatch.bindings:" + JSON.stringify(astQueryMatch.bindings) + 
+        "matched:" + JSON.stringify(matched) + " Either the matcher or the structural " +
+        "matchQuery had problems with 'typeOfThingLeftOfDot'");
+    }
+    if(matched.methodName !== node[1][2]) {
+      console.log("ERROR: Found match error in methodName: if you see this, let someone know");
+    }
+    if(astQueryMatch.bindings.callBase.methodName !== matched.methodName) {
+      console.log("ERROR: AST - astQueryMatch.bindings:" + JSON.stringify(astQueryMatch.bindings) + 
+        "matched:" + JSON.stringify(matched) + " Either the matcher or the structural " +
+        "matchQuery had problems with 'methodName'");
+    }
+
+    if(matched.args !== node[2]) {
+      console.log("ERROR: Found match error args: if you see this, let someone know");
+    }
+    if(astQueryMatch.bindings.args !== matched.args) {
+      console.log("ERROR: AST - astQueryMatch.bindings:" + JSON.stringify(astQueryMatch.bindings) + 
+        "matched:" + JSON.stringify(matched) + " Either the matcher or the structural " +
+        "matchQuery had problems with 'args'");
+    }
+  }
+  /* We'll be super liberal about what's to the left of the dot:
+   * Could have also said: // && isNonTerm(node[1][1]) && node[1][1].length === 2; */
+   return astQueryMatch.matched ? {
+     // We don't currently care about this but helpful in future
+     typeOfThingLeftOfDot: astQueryMatch.bindings.callBase.leftOfDot.typeOfThingLeftOfDot,
+     leftOfDot: astQueryMatch.bindings.callBase.leftOfDot.__node,
+     methodName: astQueryMatch.bindings.callBase.methodName,
+     args: astQueryMatch.bindings.args
+   } : null;
 }
 
 var methodDataIffTailConstruction = function(node) {
@@ -100,8 +279,11 @@ function optimizeTree (root) {
   var i;
   if(!isNonTerm(root)) { // terminal
     return root;
-  } else if (isCallNode(root) && methodDataIffTailConstruction(root)) {
-    root = swapAndOptimizeTailConstruction(root, methodDataIffTailConstruction(root));
+  }
+
+  var methodDataIffTailConstructionRes = methodDataIffTailConstruction(root);
+  if (methodDataIffTailConstructionRes) {
+    root = swapAndOptimizeTailConstruction(root, methodDataIffTailConstructionRes);
   } else if(usingStatementNodeIffUsingStatement(root)) {
     root = optimizeKnownUsingCall(root);
   } else {
