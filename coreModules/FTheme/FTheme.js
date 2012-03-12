f9e97d989b5ce702ccee5c745f7d7c749a2e80f5
jordan <jwalke@jordans-MacBook-Air.local>
2012-03-12 02:38:13 -0700

    Theme updates.

--- coreModules/FTheme/FTheme.js
+++ coreModules/FTheme/FTheme.js
@@ -21,8 +21,10 @@ var F = require('Fax'),
     FDom = require('FDom'),
     S = FDom.stylers;
 
+
 var STANDARD_LINE_HEIGHT = 14;
 var WHITE          = { r:255, g:255, b:255 };
+var BLACK          = { r:0, g:0, b:0 };
 var TEXT_DARK      = { r:51,  g:51, b:51 };
 var TEXT_SUBHEADER = { r:100,  g:100, b:100 };
 var TEXT_SUBTLE    = { r:128,  g:128, b:128 };
@@ -32,97 +34,122 @@ var SMALL = 5;
 var MEDIUM = 10;
 var LARGE = 15;
 
-var intenseBlue = { r:90, g:117, b:200 };
-var BASE         = { r:62,  g:62,  b:62 };
-var BASE_BORDER  = { r:47,  g:47,  b:47 };
-var SUB          = { r:109, g:132, b:180 };
-var TOKEN        = { r:230, g:240, b:255 };
-var TOKEN_BORDER = { r:189, g:199, b:220 };
+  var intenseBlue = { r: 114, g: 137, b: 164 };
+  var aqua = {r:118, g:169, b:161};
+  var forest = {r: 100, g:115, b:94};
+  var moss = {r: 117, g:132, b:101};
+  var redish = {r: 174, g: 81, b: 55};
+  var hlColor = forest;
 
-var CONTROL_RADIUS = 0;
+  var BASE         = S.burnDelta(hlColor, 20);
+  var BASE_BORDER  = S.burnDelta(BASE, 50);
+  var SUB          = { r:109, g:132, b:180 };
+  var TOKEN        = S.dodgeFactor(S.toneItDown(hlColor, 0.75), 0.85);
+  var TOKEN_BORDER = S.burnFactor(TOKEN, 0.15);
 
-/**
- * Box Elements.
- */
-var BOX_BLUE_BG       = { r:237, g:239, b:250 };
-var BOX_BLUE_BORDER   = { r:216, g:223, b:239 };
-var BOX_GRAY_BG       = { r:240, g:240, b:240 };
-var BOX_GRAY_BORDER   = { r:202, g:202, b:202 };
-var BOX_GRAY_LIGHT_BG = { r:246, g:246, b:246 };
-var BOX_GRAY_LIGHT_BORDER = { r:228, g:228, b:228 };
+  var CONTROL_RADIUS = 0;
 
-/* Overlay */
-var OVERLAY_BG_COLOR = WHITE;
-var OVERLAY_BORDER_COLOR = { r: 140, g: 140, b: 140 };
-var OVERLAY_BOTTOM_BORDER_COLOR = { r: 102, g: 102, b: 102 };
-var OVERLAY_SHADOW_SPEC = { r: 0, g: 0, b: 0, a: 0.3, size: 8, x: 0, y: 3};
+  /**
+   * Box Elements.
+   */
+  var BOX_BLUE_BG       = { r:237, g:239, b:250 };
+  var BOX_BLUE_BORDER   = { r:216, g:223, b:239 };
+  var BOX_GRAY_BG       = { r:240, g:240, b:240 };
+  var BOX_GRAY_BORDER   = { r:202, g:202, b:202 };
+  var BOX_GRAY_LIGHT_BG = { r:246, g:246, b:246 };
+  var BOX_GRAY_LIGHT_BORDER = { r:228, g:228, b:228 };
 
-var CURSOR_END_COLOR = intenseBlue;
-var CURSOR_START_COLOR  = S.burnDelta(intenseBlue, 10);
-var CURSOR_BORDER_COLOR = CURSOR_START_COLOR;
-var CURSOR_TEXT_COLOR   = WHITE;
+  /* Overlay: Note, we could also have overlays with regions that are not the
+   * same bgcolor as OVERLAY_BG_COLOR. */
 
-/* Conversations */
-var THREAD_BG_COLOR    = { r:240, g:240, b:240 };
-var THREAD_NEW_ITEM_COLOR = { r:200, g:200, b:200 };
-var THREAD_DIVIDER_DARK_COLOR = { r:224, g:224, b:224 };
-var THREAD_DIVIDER_LIGHT_COLOR = { r:246, g:246, b:246 };
+  var OVERLAY_BG_COLOR = WHITE;
+  var OVERLAY_BORDER_COLOR = { r: 150, g: 150, b: 150 };
+  var OVERLAY_BOTTOM_BORDER_COLOR = { r: 118, g: 118, b: 118 };
+  var OVERLAY_SHADOW_SPEC = { r: 0, g: 0, b: 0, a: 0.18, size: 8, x: 0, y: 3};
+  var OVERLAY_UP_ARROW_IMAGE_LIGHT_URI = '/staticResources/images/jordowTriangleUp_bg_255_border_150.png';
+  var OVERLAY_UP_ARROW_IMAGE_GRAY_URI = '/staticResources/images/jordowTriangleUp_bg_240_border_150.png';
 
-/* Inputs */
-var INPUT_BORDER_PROMINENT = { r:157, g:167, b:184 };
-var INPUT_BORDER_COLOR = { r: 185, g: 185, b: 185 };
-var INPUT_BACKGROUND_COLOR = { r: 255, g: 255, b: 255 };
-var INPUT_TEXT_COLOR = { r: 68, g: 68, b: 68 };
-var INPUT_TEXT_COLOR_PLACEHELD = { r: 170, g: 170, b: 170 };
+  var CURSOR_END_COLOR = hlColor;
+  var CURSOR_START_COLOR  = S.burnDelta(hlColor, 10);
+  var CURSOR_BORDER_COLOR = CURSOR_START_COLOR;
+  var CURSOR_TEXT_COLOR   = WHITE;
 
+  /* Conversations */
+  var THREAD_BG_COLOR    = { r:240, g:240, b:240 };
+  var THREAD_NEW_ITEM_COLOR = { r:200, g:200, b:200 };
+  var THREAD_DIVIDER_DARK_COLOR = { r:224, g:224, b:224 };
+  var THREAD_DIVIDER_LIGHT_COLOR = { r:246, g:246, b:246 };
 
-/**
- * Okay elements.
- */
-var OKAY_BG            = { r:238, g:238, b:238 };
-var OKAY_BG_HOVER      = S.burnDelta(OKAY_BG, 10);
-var OKAY_BG_ACTIVE     = S.burnDelta(OKAY_BG, 20);
-
-var OKAY_BORDER        = { r:165, g:165, b:165 };
-var OKAY_BORDER_HOVER  = S.burnDelta(OKAY_BORDER, 20);
-var OKAY_BORDER_ACTIVE = S.burnDelta(OKAY_BORDER, 20);
-
-var OKAY_BORDER_BOTTOM        = { r:145, g:145, b:145 };
-var OKAY_BORDER_BOTTOM_HOVER  = S.burnDelta(OKAY_BORDER_BOTTOM, 25);
-var OKAY_BORDER_BOTTOM_ACTIVE = OKAY_BORDER_ACTIVE;
-
-var OKAY_SHADOW_SPEC        = { r: 0, g: 0, b: 0, a: 0.05, size: 0, x: 0, y: 1};
-var OKAY_SHADOW_SPEC_HOVER  = { r: 0, g: 0, b: 0, a: 0.10, size: 0, x: 0, y: 1};
-var OKAY_SHADOW_SPEC_ACTIVE = { r: 0, g: 0, b: 0, a: 0.06, size: 0, x: 0, y: 1};
-
-var OKAY_TEXT_COLOR    = TEXT_DARK;
+  /* Inputs */
+  var INPUT_BORDER_PROMINENT = { r:157, g:167, b:184 };
+  var INPUT_BORDER_COLOR = S.dodgeFactor(S.toneItDown(hlColor, 0.6), 0.5);//{ r: 185, g: 185, b: 185 };
+  var INPUT_BORDER_COLOR_FOCUS = S.burnDelta(INPUT_BORDER_COLOR, 15);
+  var INPUT_BORDER_FOCUSED = { r: 185, g: 185, b: 185 };
+  var INPUT_BACKGROUND_COLOR = { r: 255, g: 255, b: 255 };
+  var INPUT_TEXT_COLOR = { r: 68, g: 68, b: 68 };
+  var INPUT_TEXT_COLOR_PLACEHELD = { r: 170, g: 170, b: 170 };
+  var INPUT_FOCUS_OUTSET_SHADOW_SPEC = F.merge(S.dodgeFactor(hlColor, 0.5), {
+    a: 0.5, size: 8, x: 0, y: 0
+  });
 
 
-/**
- * Confirm elements.
- */
-var CONFIRM_BG            = intenseBlue ;
-var CONFIRM_BG_HOVER      = S.burnDelta(CONFIRM_BG, 10);
-var CONFIRM_BG_ACTIVE     = S.burnDelta(CONFIRM_BG, 20);
+  /**
+   * Okay elements.
+   */
+  var OKAY_BG            = { r:234, g:234, b:234 };
+  var OKAY_BG_HOVER      = S.burnDelta(OKAY_BG, 8);
+  var OKAY_BG_ACTIVE     = S.burnDelta(OKAY_BG, 18);
 
-var CONFIRM_BORDER        = S.burnDelta(CONFIRM_BG, 20);
-var CONFIRM_BORDER_HOVER  = S.burnDelta(CONFIRM_BORDER, 12);
-var CONFIRM_BORDER_ACTIVE  = S.burnDelta(CONFIRM_BORDER, 12);
+  var OKAY_BORDER        = S.burnDelta(OKAY_BG, 75);
+  var OKAY_BORDER_BOTTOM = S.burnDelta(OKAY_BG, 105);
 
-var CONFIRM_BORDER_BOTTOM        = S.burnDelta(CONFIRM_BORDER, 15);
-var CONFIRM_BORDER_BOTTOM_HOVER  = S.burnDelta(CONFIRM_BORDER, 20);
-var CONFIRM_BORDER_BOTTOM_ACTIVE  = CONFIRM_BORDER_ACTIVE;
+  var OKAY_BORDER_HOVER  = S.burnDelta(OKAY_BG, 105);
+  var OKAY_BORDER_BOTTOM_HOVER = S.burnDelta(OKAY_BG, 125);
 
-var CONFIRM_SHADOW_SPEC        = { r: 0, g: 0, b: 0, a: 0.05, size: 0, x: 0, y: 1};
-var CONFIRM_SHADOW_SPEC_HOVER  = { r: 0, g: 0, b: 0, a: 0.15, size: 0, x: 0, y: 1};
-var CONFIRM_SHADOW_SPEC_ACTIVE = { r: 0, g: 0, b: 0, a: 0.08, size: 0, x: 0, y: 1};
+  var OKAY_BORDER_ACTIVE = OKAY_BORDER_HOVER;
+  var OKAY_BORDER_BOTTOM_ACTIVE = OKAY_BORDER_ACTIVE;
 
-var CONFIRM_TEXT_COLOR    = WHITE;
+  var OKAY_SHADOW_SPEC        = { a: 0.05, size: 0, x: 0, y: 1};
+  var OKAY_SHADOW_SPEC_INSET  = { a: 0.7, size: 0, x: 0, y: 1};
+  var OKAY_SHADOW_SPEC_HOVER  = { a: 0.10, size: 0, x: 0, y: 1};
+  var OKAY_SHADOW_SPEC_INSET_HOVER  = { a: 0.15, size: 0, x: 0, y: 1};
+  var OKAY_SHADOW_SPEC_ACTIVE = { a: 0.06, size: 0, x: 0, y: 1};
+  var OKAY_SHADOW_SPEC_INSET_ACTIVE = { a: 0.0, size: 0, x: 0, y: 1};
 
-var LINK = S.burnDelta(intenseBlue, 30);
+  var OKAY_TEXT_COLOR    = TEXT_DARK;
+
+
+  /**
+   * Confirm elements.
+   */
+  var CONFIRM_BG            = S.burnDelta(hlColor, 10);
+  var CONFIRM_BG_HOVER      = S.burnDelta(CONFIRM_BG, 6);
+  var CONFIRM_BG_ACTIVE     = S.burnDelta(CONFIRM_BG, 12);
+
+  var CONFIRM_BORDER        = S.burnDelta(CONFIRM_BG, 30);
+  var CONFIRM_BORDER_BOTTOM = S.burnDelta(CONFIRM_BG, 55);
+
+  var CONFIRM_BORDER_HOVER  = S.burnDelta(CONFIRM_BG, 60);
+  var CONFIRM_BORDER_BOTTOM_HOVER  = S.burnDelta(CONFIRM_BG, 75);
+
+  var CONFIRM_BORDER_ACTIVE  = CONFIRM_BORDER_HOVER;
+  var CONFIRM_BORDER_BOTTOM_ACTIVE  = CONFIRM_BORDER_HOVER;
+
+  var CONFIRM_SHADOW_SPEC        = { a: 0.07, size: 0, x: 0, y: 1};
+  var CONFIRM_SHADOW_SPEC_INSET  = { a: 0.07, size: 0, x: 0, y: 1};
+  var CONFIRM_SHADOW_SPEC_HOVER  = { a: 0.16, size: 0, x: 0, y: 1};
+  var CONFIRM_SHADOW_SPEC_INSET_HOVER  = { a: 0.07, size: 0, x: 0, y: 1};
+  var CONFIRM_SHADOW_SPEC_ACTIVE = { a: 0.08, size: 0, x: 0, y: 1};
+  var CONFIRM_SHADOW_SPEC_INSET_ACTIVE = { a: 0.0, size: 0, x: 0, y: 1};
+
+  var CONFIRM_TEXT_COLOR    = WHITE;
+
+  var LINK = S.burnDelta(hlColor, 30);
+
 
 var COLORS = {
   white: WHITE,
+  black: BLACK,
 
   grayBgColor: BOX_GRAY_BG,
   grayBorderColor: BOX_GRAY_BORDER,
@@ -167,8 +194,11 @@ var COLORS = {
   okayBorderBottomColorActive:  OKAY_BORDER_BOTTOM_ACTIVE,
 
   okayShadowSpec: OKAY_SHADOW_SPEC,
+  okayShadowSpecInset: OKAY_SHADOW_SPEC_INSET,
   okayShadowSpecHovered: OKAY_SHADOW_SPEC_HOVER,
+  okayShadowSpecInsetHovered: OKAY_SHADOW_SPEC_INSET_HOVER,
   okayShadowSpecActive: OKAY_SHADOW_SPEC_ACTIVE,
+  okayShadowSpecInsetActive: OKAY_SHADOW_SPEC_INSET_ACTIVE,
 
   okayTextColor: OKAY_TEXT_COLOR,
 
@@ -187,8 +217,11 @@ var COLORS = {
   confirmBorderBottomColorActive:  CONFIRM_BORDER_BOTTOM_ACTIVE,
 
   confirmShadowSpec: CONFIRM_SHADOW_SPEC,
+  confirmShadowSpecInset: CONFIRM_SHADOW_SPEC_INSET,
   confirmShadowSpecHovered: CONFIRM_SHADOW_SPEC_HOVER,
+  confirmShadowSpecInsetHovered: CONFIRM_SHADOW_SPEC_INSET_HOVER,
   confirmShadowSpecActive: CONFIRM_SHADOW_SPEC_ACTIVE,
+  confirmShadowSpecInsetActive: CONFIRM_SHADOW_SPEC_INSET_ACTIVE,
 
   confirmTextColor: CONFIRM_TEXT_COLOR,
 
@@ -202,16 +235,21 @@ var COLORS = {
 
   /** Text inputs */
   textInputBorderColor: INPUT_BORDER_COLOR,
+  textInputBorderColorFocus: INPUT_BORDER_COLOR_FOCUS,
   textInputBorderColorProminent: INPUT_BORDER_PROMINENT,
   textInputBackgroundColor: INPUT_BACKGROUND_COLOR,
   textInputTextColor: INPUT_TEXT_COLOR,
   textInputTextColorPlaceheld: INPUT_TEXT_COLOR_PLACEHELD,
+  textInputFocusOutsetShadowSpec: INPUT_FOCUS_OUTSET_SHADOW_SPEC,
 
   /* Overlays */
   overlayBgColor: OVERLAY_BG_COLOR,
   overlayBorderColor: OVERLAY_BORDER_COLOR,
   overlayBottomBorderColor: OVERLAY_BOTTOM_BORDER_COLOR,
   overlayBoxShadow: OVERLAY_SHADOW_SPEC,
+
+  overlayUpArrowImageLightUri: OVERLAY_UP_ARROW_IMAGE_LIGHT_URI,
+  overlayUpArrowImageGrayUri: OVERLAY_UP_ARROW_IMAGE_GRAY_URI,
 
   small: SMALL,
   medium: MEDIUM,
@@ -221,7 +259,7 @@ var COLORS = {
 var DIMS = {
   standardLineHeight: 14,
   controlsRadius: CONTROL_RADIUS,
-  controlsFontSize: 11,
+  controlsFontSize: 12,
   textInputFontSize: 12,
   controlsHeight: 24,
   borderThick: 1
